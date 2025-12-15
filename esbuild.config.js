d611ebc2ca29d6c7d825ad6a1ff87fc1ac42be0c
Dmitri Dmitriev <d.dzmitryeu@gmail.com>
2025-12-15 16:16:56 +0200

    refactor esbuild configuration for improved readability

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,15 +1,20 @@
-const esbuild = require('esbuild');
+const esbuild = require("esbuild");
 
-esbuild.build({
-  entryPoints: ['src/code.ts'],
-  bundle: true,
-  outfile: 'dist/code.js',
-  platform: 'browser',
-  target: 'es2017',
-  format: 'iife',
-  logLevel: 'info',
-  minify: true,
-  define: {
-    'process.env.NODE_ENV': '"production"',
-  },
-}).catch(() => process.exit(1));
+esbuild
+  .build({
+    entryPoints: ["src/code.ts"],
+    bundle: true,
+    outfile: "dist/code.js",
+    platform: "browser",
+    target: "es2017",
+    format: "iife",
+    logLevel: "info",
+    minify: true,
+    loader: {
+      ".svg": "dataurl",
+    },
+    define: {
+      "process.env.NODE_ENV": '"production"',
+    },
+  })
+  .catch(() => process.exit(1));
